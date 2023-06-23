16892196c61339e7557ed8c954a04c2b912e6d63
Marc Haubenstock <marc.dhaubenstock@gmail.com>
2023-06-23 21:48:47 +0200

    Better loading for buffer data

---
+++ src/byte_array_info.rs
@@ -0,0 +1,37 @@
+extern crate gltf;
+use gltf::accessor::{DataType,Dimensions};
+
+pub struct ByteArrayInfo {
+    data_type: DataType, 
+    data_dimension: Dimensions,
+    buffer_index: usize,
+    byte_offset: usize,
+    byte_length: usize,
+    byte_stride: Option<usize>
+}
+
+impl ByteArrayInfo {
+    pub fn new(
+        data_type: DataType, 
+        data_dimension: Dimensions,
+        buffer_index: usize,
+        byte_offset: usize,
+        byte_length: usize,
+        byte_stride: Option<usize>) -> ByteArrayInfo {
+            ByteArrayInfo {
+                data_type,
+                data_dimension,
+                buffer_index,
+                byte_offset,
+                byte_length,
+                byte_stride
+            }
+        }
+
+    pub fn get_data_type(&self) -> DataType {self.data_type}
+    pub fn get_data_dimension(&self) -> Dimensions {self.data_dimension}
+    pub fn get_buffer_index(&self) -> usize {self.buffer_index}
+    pub fn get_byte_offset(&self) -> usize {self.byte_offset}
+    pub fn get_byte_length(&self) -> usize {self.byte_length}
+    pub fn get_byte_stride(&self) -> Option<usize> {self.byte_stride}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,28 @@
-use std::collections::HashSet;
 extern crate gltf;
+mod byte_array_info;
+
+use byte_array_info::ByteArrayInfo;
+
 
 /**
- * Returns (buffer index, offset,length,Option<stride>) of position buffers
+ * Returns (data type, data dimension, buffer index, byte offset,byte length, option of byte stride) of position buffers
  */
-pub fn find_position_buffer_data(document: &gltf::Document) -> Vec<(usize, usize, usize,Option<usize>)> { 
-    // meshes -> primitives -> attributes
-    let position_attribute_indices : HashSet<_> = document.meshes().map(|mesh| {
+pub fn find_position_buffer_data(document: &gltf::Document) -> Vec<ByteArrayInfo> { 
+    document.meshes().map(|mesh| {
         mesh.primitives().map(|primitive| {
-            primitive.attributes().filter(|attribute| attribute.0 == gltf::Semantic::Positions).map(|attr| attr.1.index())
+            primitive.attributes().filter(|attribute| attribute.0 == gltf::Semantic::Positions).map(|attr| {
+                let buffer_view = attr.1.view().expect("Buffer is sparse. This is not implemented");
+                ByteArrayInfo::new (attr.1.data_type(), attr.1.dimensions(), buffer_view.buffer().index(),buffer_view.offset(), buffer_view.length(), buffer_view.stride())
+            })
         }).flatten()
-    }).flatten().collect();
-    document.views().enumerate().filter(|(i,_)| position_attribute_indices.contains(i)).map(|(_,v)| (v.buffer().index(),v.offset(),v.length(),v.stride())).collect()
+    }).flatten().collect()
 }
 
-pub fn load_position_vectors(position_buffer_info: Vec<(usize, usize, usize,Option<usize>)>, buffers: &Vec<gltf::buffer::Data>) -> Vec<&[u8]> {
-    position_buffer_info.into_iter().map(|(buffer_index,offset,length,option_stride)| {
-        let byte_end = offset+length;
-        match option_stride {
-            None => &buffers[buffer_index].0[offset..byte_end],
+pub fn load_position_vectors(position_buffer_info: Vec<ByteArrayInfo>, buffers: &Vec<gltf::buffer::Data>) -> Vec<&[u8]> {
+    position_buffer_info.into_iter().map(|info| {
+        let byte_end = info.get_byte_offset()+info.get_byte_length();
+        match info.get_byte_stride() {
+            None => &buffers[info.get_buffer_index()].0[info.get_byte_offset()..byte_end],
             Some(_) => panic!("TODO Strided Position Loading") // After every read, continue for stide - size bytes
         }
     }).collect()
