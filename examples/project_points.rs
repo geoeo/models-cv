c010230b871aca2b7bbb0a71caf32edb29f667a3
Marc Haubenstock <marc.dhaubenstock@gmail.com>
2023-07-02 15:21:32 +0200

    moved png crate to dev dep

--- examples/project_points.rs
+++ examples/project_points.rs
@@ -1,5 +1,10 @@
 extern crate nalgebra as na;
 
+use std::path::Path;
+use std::fs::File;
+use std::io::BufWriter;
+use std::result::Result;
+use png::EncodingError;
 use na::{Vector3,Isometry3,Point3, Matrix3};
 
 fn main() {
@@ -58,7 +63,34 @@ fn project_points(points: &Vec<Vector3<f32>>) -> () {
         let visible_screen_points = visible_points_for_cam.iter().map(|&(_,v)| v).collect::<Vec<_>>();
         let data_vec = models_cv::calculate_rgb_byte_vec(&visible_screen_points, screen_width as usize, screen_height as usize);
         let name = format!("/home/marc/Workspace/Rust/models-cv/output/test_suzanne_{}.png",camera_id+1);
-        models_cv::io::write_data_to_file(name.as_str(), &data_vec,screen_width as u32, screen_height as u32).expect("Writing png failed!");
+        write_png_data_to_file(name.as_str(), &data_vec,screen_width as u32, screen_height as u32).expect("Writing png failed!");
+        write_test_png();
     }
 
+}
+
+fn write_png_data_to_file(path_str: &str, data_vec: &Vec<u8>, screen_width: u32, screen_height: u32) -> Result<(),EncodingError> {
+    let path = Path::new(path_str);
+    let file = File::create(path).unwrap();
+    let ref mut w = BufWriter::new(file);
+
+    let mut encoder = png::Encoder::new(w, screen_width, screen_height); 
+    encoder.set_color(png::ColorType::Rgb);
+    encoder.set_depth(png::BitDepth::Eight);
+    encoder.set_source_gamma(png::ScaledFloat::new(1.0 / 2.2));     // 1.0 / 2.2, unscaled, but rounded
+    let source_chromaticities = png::SourceChromaticities::new(     // Using unscaled instantiation here
+        (0.31270, 0.32900),
+        (0.64000, 0.33000),
+        (0.30000, 0.60000),
+        (0.15000, 0.06000)
+    );
+    encoder.set_source_chromaticities(source_chromaticities);
+    let mut writer = encoder.write_header().unwrap();
+
+    writer.write_image_data(&data_vec[..]) // Save
+}
+
+fn write_test_png() -> () {
+    let data_vec = vec![255, 0, 0, 0, 0, 0, 0, 255 ,0, 0,0,255]; // An array containing a RGB sequence. First pixel is red and second pixel is black.
+    write_png_data_to_file("/home/marc/Workspace/Rust/models-cv/output/test.png",&data_vec,2,2).unwrap();
 }
