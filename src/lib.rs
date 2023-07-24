ed81cca1d4cfaf9573a9a39ec36c6c2b56d56a3a
Marc Haubenstock <marc.dhaubenstock@gmail.com>
2023-07-24 22:15:17 +0200

    points to triangle

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,10 @@ pub mod triangle;
 use std::iter::zip;
 use std::collections::HashMap;
 use na::{Vector2,Vector3,Matrix3,Matrix4xX,Matrix3x4, Matrix3xX};
+use triangle::Triangle;
 
 /**
- * Returns A vector of indexed points in image space where the index represents the column of the corresponding 3D point matrix in world/camera space
+ * Returns A vector of indexed points in image space where the index represents the column of the corresponding 3D point matrix in camera space
  */
 pub fn project_points(points: &Vec<Vector3<f32>>, intrinsic_matrix: &Matrix3<f32>, view_matrix: &Matrix3x4<f32>, screen_width: f32, screen_height: f32) -> (Vec<(usize,Vector2<usize>)>, Matrix3xX<f32>) {
     let mut ps = Matrix4xX::<f32>::from_element(points.len(), 1.0);
@@ -29,6 +30,20 @@ pub fn project_points(points: &Vec<Vector3<f32>>, intrinsic_matrix: &Matrix3<f32
         .map(|(i,(x,y))| (i,Vector2::new(x.floor() as usize, y.floor() as usize)))
         .collect::<Vec<_>>();
     (screen_points_with_idx, points_cam)
+}
+
+pub fn group_points_to_triangles(points: &(Vec<(usize,Vector2<usize>)>, Matrix3xX<f32>)) -> Vec<(Triangle<2>,Triangle<3>)> {
+    let pixels = &points.0;
+    let cam_points = &points.1;
+    (0..pixels.len()-2).map(|i| {
+        let (id_0,pix_v0) = pixels[i];
+        let (id_1,pix_v1) = pixels[i+1];
+        let (id_2,pix_v2) = pixels[i+1];
+        let cam_point_v0 = cam_points.column(id_0);
+        let cam_point_v1 = cam_points.column(id_1);
+        let cam_point_v2 = cam_points.column(id_2);
+        (Triangle::from_vec(&pix_v0.cast::<f32>(),&pix_v1.cast::<f32>(),&pix_v2.cast::<f32>()),Triangle::from_view(&cam_point_v0, &cam_point_v1, &cam_point_v2))
+    }).collect::<Vec<_>>()
 }
 
 pub fn filter_screen_points_for_camera_views(points: &Vec<Vector3<f32>>, intrinsic_matrix: &Matrix3<f32>, view_matrices: &Vec<Matrix3x4<f32>>, screen_width: f32, screen_height: f32, filter_type: filter::FilterType) -> Vec<Vec<(usize,Vector2<usize>)>> {
